0ab990e683c8751cd661097de892739b957fbe18
Dries Nicolay <dries@MacBook-Pro-van-Dries.local>
2017-03-11 11:05:41 +0100

    Added weather card.

--- src/app/app.component.js
+++ src/app/app.component.js
@@ -11,12 +11,12 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
-        this.name = 'Angular';
+        this.weatherInfo = { temperature: '20' };
     }
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: "<h1>Weer {{name}}</h1>",
+            templateUrl: 'app/app.component.html',
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Weer {{name}}</h1>`,
+  templateUrl: 'app/app.component.html',
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  weatherInfo = {temperature:'20'}; 
+}
