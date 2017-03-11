d299e26e48508249a8e1396c6f96a19979252f5c
Dries Nicolay <dries@MacBook-Pro-van-Dries.local>
2017-03-11 10:17:05 +0100

    Include angular-material.

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -8,15 +8,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
+require('hammerjs');
 var core_1 = require('@angular/core');
 var platform_browser_1 = require('@angular/platform-browser');
+var material_1 = require('@angular/material');
 var app_component_1 = require('./app.component');
 var AppModule = (function () {
     function AppModule() {
     }
     AppModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule],
+            imports: [platform_browser_1.BrowserModule, material_1.MaterialModule],
             declarations: [app_component_1.AppComponent],
             bootstrap: [app_component_1.AppComponent]
         }), 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
+import 'hammerjs';
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from '@angular/material';
 
 import { AppComponent }  from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, MaterialModule ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
